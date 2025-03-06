8bb3ce470e82c68a0acc66e0e9c4d8fdb9977417
Julian Velandia <julianvelandia3110@gmail.com>
2025-03-06 00:53:18 -0500

    feat:componentes navbar, slide, footer y card creados y añadidos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { SlideComponent } from './components/slide/slide.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardComponent } from './components/card/card.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SlideComponent,
+    NavbarComponent,
+    CardComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
