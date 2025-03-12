7c3ae1cb633844e350e897850c4319300cf64fd0
Julian Velandia <julianvelandia3110@gmail.com>
2025-03-11 22:01:54 -0500

    feat: loggin y register creados junto con sus componentes

---
+++ src/app/components/form-loggin/form-loggin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-loggin',
+  imports: [],
+  templateUrl: './form-loggin.component.html',
+  styleUrl: './form-loggin.component.css'
+})
+export class FormLogginComponent {
+
+}
---
+++ src/app/components/navbar-loggin/navbar-loggin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-loggin',
+  imports: [],
+  templateUrl: './navbar-loggin.component.html',
+  styleUrl: './navbar-loggin.component.css'
+})
+export class NavbarLogginComponent {
+
+}
